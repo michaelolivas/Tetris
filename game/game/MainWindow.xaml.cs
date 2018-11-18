050f2bdd46308ba2660f711c378362079d676b32
Rigoberto Macedo <rigomacedo0629@gmail.com>
2018-11-18 15:32:36 -0800

    created z, square init funcs

--- game/game/MainWindow.xaml.cs
+++ game/game/MainWindow.xaml.cs
@@ -37,11 +37,86 @@ namespace game
 
              GridOne.Children.Add(one);*/
 
-            Grid bar = create_bar();
+            Grid bar = create_z();
             GridOne.Children.Add(bar);
             bar.Margin = new Thickness(0, 120, 0, 0);
             bar.TranslatePoint(new Point(40,40),bar);
 
+        }
+        Grid create_z()
+        {
+            Grid z = new Grid();
+
+            Rectangle one = new Rectangle();
+            one.Height = 20;
+            one.Width = 20;
+            one.Fill = Brushes.LightGreen;
+            one.Stroke = Brushes.Black;
+
+            Rectangle two = new Rectangle();
+            two.Height = 20;
+            two.Width = 20;
+            two.Fill = Brushes.LightGreen;
+            two.Stroke = Brushes.Black;
+            two.Margin = new Thickness(-40, 0, 0, 0);
+
+            Rectangle three = new Rectangle();
+            three.Height = 20;
+            three.Width = 20;
+            three.Fill = Brushes.LightGreen;
+            three.Stroke = Brushes.Black;
+            three.Margin = new Thickness(0, 0, 0, -40);
+
+            Rectangle four = new Rectangle();
+            four.Height = 20;
+            four.Width = 20;
+            four.Fill = Brushes.LightGreen;
+            four.Stroke = Brushes.Black;
+            four.Margin = new Thickness(0, 0, -40, -40);
+
+            z.Children.Add(one);
+            z.Children.Add(two);
+            z.Children.Add(three);
+            z.Children.Add(four);
+            return z;
+        }
+
+        Grid create_square()
+        {
+            Grid square = new Grid();
+
+            Rectangle one = new Rectangle();
+            one.Height = 20;
+            one.Width = 20;
+            one.Fill = Brushes.Yellow;
+            one.Stroke = Brushes.Black;
+
+            Rectangle two = new Rectangle();
+            two.Height = 20;
+            two.Width = 20;
+            two.Fill = Brushes.Yellow;
+            two.Stroke = Brushes.Black;
+            two.Margin = new Thickness(0, -40, 0, 0);
+
+            Rectangle three = new Rectangle();
+            three.Height = 20;
+            three.Width = 20;
+            three.Fill = Brushes.Yellow;
+            three.Stroke = Brushes.Black;
+            three.Margin = new Thickness(0, -40, 40, 0);
+
+            Rectangle four = new Rectangle();
+            four.Height = 20;
+            four.Width = 20;
+            four.Fill = Brushes.Yellow;
+            four.Stroke = Brushes.Black;
+            four.Margin = new Thickness(0, 0, 40,0);
+
+            square.Children.Add(one);
+            square.Children.Add(two);
+            square.Children.Add(three);
+            square.Children.Add(four);
+            return square;
         }
         Grid create_bar()
         {
