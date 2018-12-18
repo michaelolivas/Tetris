3105f8ad0c4deccd6820a74dcafa32ec5f0e7134
LeoPeralta <LeoPeralta@LEOPERALTAAAA7>
2018-12-17 16:56:55 -0800

    Broken Collision logic

--- game/TetrisMUWP/Game1.cs
+++ game/TetrisMUWP/Game1.cs
@@ -22,27 +22,19 @@ namespace TetrisMUWP
         float screenWidth;
         float screenHeight;
         Texture2D grass;
-        Texture2D teeBar;
-        bool color = true;
-        bool flag;
+        bool flag = true;
         const int blockSize = 50;
-        int ypos = 0;
-        int xpos = 200;
-        int boardxpos = 100;
-        int boardypos = 100;
-
+        int ypos = 0; //Block y
+        int xpos = 0; //Block x
+        int boardxpos = 0;
+        int boardypos = 0;
+        bool falling = true;
         const int boardX = 10;
         const int boardY = 18;
+        int Position_Period = 300;
+        int Period_Counter = 0;
+        Random rnd = new Random();
 
-        int row = 4;
-        int column = 4;
-        int[,] test_block;
-        int[,] modified_field;
-        bool rotate = false;
-        bool falling = true;
-        bool overflow = false;
-        int middle = 4;
-        int i = 0;
 
         List<int[,]> Blocks = new List<int [,]>();
         Color[] Block_Color = {Color.Transparent, Color.Cyan, Color.Purple, Color.Orange, Color.Blue,
@@ -54,9 +46,16 @@ namespace TetrisMUWP
         int[,] Z = new int[3, 3] { { 0, 0, 0 }, { 0, 5, 5 }, { 5, 5, 0 } };
         int[,] Backwards_Z = new int[3, 3] { { 0, 0, 0 }, { 5, 5, 0 }, { 0, 5, 5} };
         int[,] Box = new int[2, 2] { { 6, 6 }, { 6, 6 } };
+        int[,] Rand_Piece = null;
 
-        Game_Grid Field;
-
+        int [,] Field = new int[boardY, boardX];
+        Vector2 FieldLocation = Vector2.Zero;
+        Vector2 BlockLocation = Vector2.Zero;
+        bool Rotate = false;
+        bool Overflow = false;
+        int middle = 4;
+        int Backend_Falling = 0;
+        int[,] test_block; 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -72,11 +71,16 @@ namespace TetrisMUWP
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            for(int y=0; y < boardY; y++)
+            {
+                for(int x= 0; x< boardX; x++)
+                {
+                    Field[y, x] = 0;
+                }
+            }
+            
             screenHeight = (float)ApplicationView.GetForCurrentView().VisibleBounds.Height;
             screenWidth = (float)ApplicationView.GetForCurrentView().VisibleBounds.Width;
-
-            Field = new Game_Grid();
-
             Blocks.Add(Line);
             Blocks.Add(T);
             Blocks.Add(L);
@@ -84,6 +88,8 @@ namespace TetrisMUWP
             Blocks.Add(Z);
             Blocks.Add(Backwards_Z);
             Blocks.Add(Box);
+            Rand_Piece = (int[,])Blocks[rnd.Next(1, Blocks.Count)].Clone();
+
             // test_block = Field.original_block(Line, row, column);
             //modified_field = Field.Solid_Field();
 
@@ -91,7 +97,45 @@ namespace TetrisMUWP
             base.Initialize();
             previousState = Keyboard.GetState();
         }
+        public bool Collision(int x, int y)
+        {
+            for(int BlockY = 0; BlockY < Rand_Piece.GetLength(0); BlockY++)
+            {
 
+                for (int BlockX = 0; BlockX < Rand_Piece.GetLength(0); BlockX++)
+                {
+                    int next_blockY = y + BlockY;
+                    int next_blockX = x + BlockX;
+                    if(Rand_Piece[BlockY, BlockX] != 0)
+                    {
+                        if(next_blockX<0 ||next_blockX >= 10)
+                        {
+                            return true;
+                        }
+                    }
+                    if (next_blockY >=18  || Field[next_blockY, next_blockX] != 0)
+                    {
+                        return true;
+                    } 
+
+                }
+                
+            }
+            return false;
+        }
+        public void Paste(int Fieldx, int Fieldy)
+        {
+            for(int y=0; y < Rand_Piece.GetLength(0); y++)
+            {
+                for(int x=0; x<Rand_Piece.GetLength(0); x++)
+                {
+                    int pasteX = Fieldx +x;
+                    int pasteY = Fieldy+y;
+                    if(Rand_Piece[y,x] !=0)
+                        Field[pasteY-2, pasteX] = Rand_Piece[y, x];
+                }
+            }
+        }
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
@@ -108,18 +152,18 @@ namespace TetrisMUWP
         void KeyboardHandler()
         {
             KeyboardState state = Keyboard.GetState();
-            Debug.WriteLine("x:" + xpos);
-            Debug.WriteLine("y:" + ypos);
+            //Debug.WriteLine("x:" + xpos);
+            //Debug.WriteLine("y:" + ypos);
             if (state.IsKeyDown(Keys.Right) && !previousState.IsKeyDown(Keys.Right))
             {
-                if(xpos < boardxpos + 10*blockSize-blockSize)
-                xpos += blockSize;
+                if (!Collision((int)BlockLocation.X, (int)BlockLocation.Y))
+                    BlockLocation += new Vector2(0,-1);
             }
             if (state.IsKeyDown(Keys.Left) && !previousState.IsKeyDown(Keys.Left))
             {
 
-                if(xpos> boardxpos)
-                xpos -= blockSize;
+                if(!Collision((int)BlockLocation.X, (int)BlockLocation.Y))
+                    BlockLocation += new Vector2(0,1);
             }
             if (state.IsKeyDown(Keys.Space))
             {
@@ -145,32 +189,43 @@ namespace TetrisMUWP
         /// 
         protected override void Update(GameTime gameTime)
         {
-            //Line = Field.original_block(Line, row, column);
-            //test_block = Field.original_block(Line, row, column);
-            //falling = Field.Falling_Block(Line, row, column, test_block, modified_field, rotate, falling, overflow, middle, i);
-            //i++;
-
-            //ypos = row * blockSize;
             // TODO: Add your update logic here
+
             flag = false;
+
+            Period_Counter += gameTime.ElapsedGameTime.Milliseconds;
+
             KeyboardHandler();
-            if (ypos < boardypos + 18 * blockSize - blockSize) {
+            if (Period_Counter >= Position_Period)
+            {
+                Vector2 NextSpot = BlockLocation + new Vector2(0, 1);
+                if (Collision((int)BlockLocation.X, (int)BlockLocation.Y))
+                {
+                    Paste((int)BlockLocation.X, (int)BlockLocation.Y);
+                    Rand_Piece = (int[,])Blocks[rnd.Next(1, Blocks.Count)].Clone();
+                    BlockLocation = Vector2.Zero;
+                }
+                BlockLocation = NextSpot;
+                Period_Counter = 0;
+            }
+            
+            /*if (ypos < boardypos + 18 * blockSize - blockSize - blockSize * Rand_Piece.GetLength(0)) {
                 ypos += 1;
-                if (ypos == 499)
+                if (ypos == 18*blockSize-blockSize)
                 {
                     flag = true;
                 }
             }
-            if (ypos == boardypos + 18 * blockSize - blockSize)
+            if (ypos == boardypos + 18 * blockSize - blockSize-blockSize * Rand_Piece.GetLength(0))
             {
                 Debug.WriteLine("Collision!");
                 Debug.WriteLine("x:" + xpos);
                 Debug.WriteLine("y:" + ypos);
                 Debug.WriteLine((int)(((xpos - boardxpos) + blockSize) / blockSize) - 1);
                 Debug.WriteLine((int)((ypos - boardypos + blockSize) / blockSize) - 2);
-                Field.field[(int) ((ypos - boardypos + blockSize) / blockSize)- 1,(int)(((xpos - boardxpos) + blockSize) / blockSize) - 1] = 1;
+                Field.field[(int) ((ypos - boardypos + blockSize + blockSize*Rand_Piece.GetLength(0)) / blockSize)- 1,(int)(((xpos - boardxpos) + blockSize) / blockSize) - 1] = 1;
                 ypos = 0;
-            }
+            }*/
             base.Update(gameTime);
         }
 
@@ -182,37 +237,38 @@ namespace TetrisMUWP
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            //int num = new Random().Next(1, 2);
-            //spriteBatch.Begin();
-            //shapes();
-            //spriteBatch.End();
-
             // TODO: Add your drawing code here
             spriteBatch.Begin();
             for (int y = 0; y < fieldRow; y++)
             {
                 for (int x = 0; x < fieldColumn; x++)
                 {
-                    Color currColor = Color.FromNonPremultiplied(50,50,50,50);
-                    Color Field_Color = Block_Color[Field.field[y, x]];
-                    //Color tintColor = TetronimoColors[Board[x, y]];
-                    if (Field.field[y, x] == 1)
+                    Color Field_Color = Block_Color[Field[y, x]];
+                    if (Field[y, x] == 0 )
                     {
-                        currColor = Color.Red;
+                        Field_Color = Color.FromNonPremultiplied(50, 50, 50, 50);
                     }
                     // Since for the board itself background colors are transparent, we'll go ahead and give this one
                     // a custom color.  This can be omitted if you draw a background image underneath your board
-
-                    spriteBatch.Draw(grass, new Rectangle(boardxpos + x * blockSize, boardypos + y * blockSize, blockSize, blockSize), currColor);
+                    spriteBatch.Draw(grass, new Rectangle((int)FieldLocation.X + x * blockSize, (int)FieldLocation.Y + y * blockSize, blockSize, blockSize), Field_Color);
                 }
             }
-            spriteBatch.Draw(grass, new Rectangle(xpos, ypos, blockSize, blockSize), Color.White);
-            spriteBatch.Draw(grass, new Rectangle(xpos, ypos, blockSize, blockSize), Color.White);
-            spriteBatch.Draw(grass, new Rectangle(xpos + blockSize, ypos, blockSize, blockSize), Color.White);
-            spriteBatch.Draw(grass, new Rectangle(xpos, ypos - blockSize, blockSize, blockSize), Color.White);
-            spriteBatch.Draw(grass, new Rectangle(xpos + blockSize, ypos - blockSize, blockSize, blockSize), Color.White);
+           
+            for (int y = 0; y < Rand_Piece.GetLength(0); y++)
+            {
+                for (int x = 0; x < Rand_Piece.GetLength(0); x++)
+                {
+                    if(Rand_Piece[x,y] != 0)
+                        spriteBatch.Draw(grass, new Rectangle((int)FieldLocation.X + ((int)BlockLocation.X + x) * blockSize,
+                                                              (int)FieldLocation.Y + ((int)BlockLocation.Y + y) * blockSize, blockSize, blockSize),
+                                                               Block_Color[Rand_Piece[x, y]]);
+
+                } 
+            }
+            
             spriteBatch.End();
             base.Draw(gameTime);
+
 
         }
     }
